c6296f5bf5e7d7e82781bf6ee140f2b6b0c7505c
letszun <letszun@gmail.com>
2026-08-13 11:05:37 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,17 +1,13 @@
-const CACHE='cq-trip-v48-function-map-hotel';
-const ASSETS=['./','./index.html','./manifest.json','./images/jiefangbei.svg','./images/hongyadong.svg','./images/bayi.svg','./images/tmpgov.svg','./images/tiansheng.svg','./images/fairymountain.svg','./images/longshuixia.svg','./images/guanyinqiao.svg','./images/jiujie.svg','./images/luohansi.svg','./images/ciqikou.svg','./images/liziba.svg','./images/ering.svg','./images/shibati.svg','./images/xiahao.svg','./images/milktea.svg','./images/yangrou.svg','./images/kaoyu.svg','./images/xiaomian.svg','./images/xiaolongxia.svg','./images/dimsum.svg','./images/furong.svg','./images/fallback.svg'];
+const CACHE='cq-trip-v55-detail-contrast';
+const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(
-    caches.match(e.request).then(cached=>cached||fetch(e.request).then(r=>{
-      const copy=r.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,copy));
-      return r;
-    }).catch(()=>{
-      if(e.request.destination==='image') return caches.match('./images/fallback.svg');
-      return caches.match('./index.html');
-    }))
-  );
+  if(e.request.method!=='GET')return;
+  if(e.request.mode==='navigate'){
+    e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',c));return r}).catch(()=>caches.match('./index.html')));
+    return;
+  }
+  if(new URL(e.request.url).origin!==self.location.origin)return;
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
