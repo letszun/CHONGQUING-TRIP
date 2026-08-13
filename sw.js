c7621b91159f9e28bc9f71214b62f4812a40e65c
letszun <letszun@gmail.com>
2026-08-13 09:03:55 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cq-trip-v45-pastel-box-map';
+const CACHE='cq-trip-v46-ui-clean';
 const ASSETS=['./','./index.html','./manifest.json','./images/jiefangbei.svg','./images/hongyadong.svg','./images/bayi.svg','./images/tmpgov.svg','./images/tiansheng.svg','./images/fairymountain.svg','./images/longshuixia.svg','./images/guanyinqiao.svg','./images/jiujie.svg','./images/luohansi.svg','./images/ciqikou.svg','./images/liziba.svg','./images/ering.svg','./images/shibati.svg','./images/xiahao.svg','./images/milktea.svg','./images/yangrou.svg','./images/kaoyu.svg','./images/xiaomian.svg','./images/xiaolongxia.svg','./images/dimsum.svg','./images/furong.svg','./images/fallback.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
