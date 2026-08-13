94af2ecdee2a87bdbb72819e9d978d6b83a0c232
letszun <letszun@gmail.com>
2026-08-13 11:44:30 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='cq-trip-v58-discovery-silence';
+const CACHE='cq-trip-v59-meals';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
